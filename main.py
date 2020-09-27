6e0d52f39068b049f1ac31959bcfa611f8a0e959
UZA <spb.serega2003@yandex.ru>
2020-09-27 20:48:19 +0300

    6

--- main.py
+++ main.py
@@ -36,12 +36,17 @@ def help_message(message):
     bot.send_message(message.chat.id, "https://python-scripts.com/sqlite")
     bot.send_message(message.chat.id, "==============================================================")
     bot.send_message(message.chat.id, "https://www.severcart.ru/blog/all/python_sqlite3/")
+    bot.send_message(message.chat.id, "==============================================================")
+    bot.send_message(message.chat.id, "https://tlgrm.ru/docs/bots/api")
+    bot.send_message(message.chat.id, "==============================================================")
+    bot.send_message(message.chat.id,"https://sebastianraschka.com/Articles/2014_sqlite_in_python_tutorial.html")
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text == 'привет':
         bot.send_message(message.chat.id, 'Привет, мой создатель')
+        bot.send_message(message.chat.id,)
     if message.text == 'тест':
         bot.send_message(message.chat.id, str(message.text()))
     elif message.text == 'пока':
@@ -77,8 +82,8 @@ def get_pas(message):
     global pas
     pas=message.text
 
-    bot.send_message(message.from_user.id,"Твой логин " + login)
-    bot.send_message(message.from_user.id, "Твой пароль " + pas)
+    bot.send_message(message.from_user.id,"Твой логин " + str(login))
+    bot.send_message(message.from_user.id, "Твой пароль " + str(pas))
 
 
     markup = types.InlineKeyboardMarkup(row_width=1)
@@ -86,9 +91,35 @@ def get_pas(message):
     no_btn = types.InlineKeyboardButton(text='Нет',callback_data="Неа")
     markup.add(yes_btn)
     markup.add(no_btn)
-    bot.send_message(message.chat.id, "Всё верно?",)
+    bot.send_message(message.chat.id, "Всё верно?",reply_markup=markup)
 
-# def reg_login(message):
+def reg_login(message):
+    bd = sqlite3.connect(r'./Logins/bd')
+    cursor = bd.cursor()
+    cursor.execute("""SELECT * FROM logins WHERE ID LIKE '{sravnenie}' """.format(sravnenie=str(message.chat.id)))
+    print(message.chat.id)
+    srav=cursor.fetchall()
+    print(srav)
+    if str(srav)=="[]":
+        print("Create User")
+        cursor.execute("""INSERT INTO logins (ID) VALUES (?)""", [message.chat.id])
+    cursor.execute("""UPDATE logins SET login = "{log}" where id = {id}""".format(log=message.text,id=message.chat.id))
+    print("update")
+    bd.commit()
+    bot.send_message(message.chat.id,"Ваш пароль?")
+    bot.register_next_step_handler(message, reg_pas)
 
+
+def reg_pas(message):
+    bd = sqlite3.connect(r'./Logins/bd')
+    cursor = bd.cursor()
+    cursor.execute("""SELECT * FROM logins WHERE ID LIKE '{sravnenie}' """.format(sravnenie=str(message.chat.id)))
+    print(message.chat.id)
+    srav = cursor.fetchall()
+    print(srav)
+    cursor.execute("""UPDATE logins SET password = "{pas}" where id = {id}""".format(pas=message.text, id=message.chat.id))
+    print("update")
+    bd.commit()
+    bot.send_message(message.chat.id,"Регистрация выполнена")
 
 bot.polling()
